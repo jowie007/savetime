5af0eefc451410969c2c51fbb5fc2948b21137c5
jowie007 <90611576+jowie007@users.noreply.github.com>
2022-11-05 12:55:17 +0100

    removed browserwindow attribute

--- cypress/support/savetime/src/main.ts
+++ cypress/support/savetime/src/main.ts
@@ -8,7 +8,6 @@ function createWindow() {
     width: 800,
     height: 600,
     webPreferences: {
-      contextIsolation: true,
       sandbox: false,
       preload: path.join(__dirname, 'preload.js'),
     },
