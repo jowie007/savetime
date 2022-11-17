258fe194308c18373b70d613240f268b345d693a
Jonas Wiegmann <jonas.wiegmann@adesso.de>
2022-11-17 13:22:22 +0100

    started implementing datepicker heading

--- cypress/support/savetime/src/services/printer/datepicker-printer.ts
+++ cypress/support/savetime/src/services/printer/datepicker-printer.ts
@@ -1,6 +1,8 @@
 import { getAllFileDetails } from "../handler/cypress-file-handler";
+import { translation } from "../handler/translation-handler";
 
-let selection__datepicker: HTMLDivElement;
+let selection__datepicker__heading: HTMLElement;
+let selection__datepicker__wrapper: HTMLDivElement;
 let files: Map<number, Date>;
 
 export function printDatepicker() {
@@ -9,10 +11,37 @@ export function printDatepicker() {
 
 function init() {
   files = getAllFileDetails();
-  selection__datepicker = document.getElementById(
-    "selection__datepicker"
+  selection__datepicker__heading = document.getElementById(
+    "selection__datepicker__heading"
+  ) as HTMLElement;
+  selection__datepicker__wrapper = document.getElementById(
+    "selection__datepicker__wrapper"
   ) as HTMLDivElement;
-  selection__datepicker.innerHTML = getDatePickerContent(2022, 10);
+  selection__datepicker__heading.innerHTML = getDatePickerTitle(2022, 10);
+  selection__datepicker__wrapper.innerHTML = getWeekdayContent();
+  selection__datepicker__wrapper.innerHTML += getDatePickerContent(2022, 10);
+}
+
+function getDatePickerTitle(year: number, month: number) {
+  return (
+    `<button 
+        id='selection__datepicker__navigation__back'
+        class='selection__datepicker__navigation'
+        />
+        ${translation.selection__datepicker__navigation__back}
+    </button>` +
+    `<h2>${
+      translation.months.split(",")[month] +
+      " " +
+      new Date(year, month).getFullYear()
+    }</h2>` +
+    `<button 
+        id='selection__datepicker__navigation__forwards'
+        class='selection__datepicker__navigation'
+        />
+        ${translation.selection__datepicker__navigation__forwards}
+    </button>`
+  );
 }
 
 function getDayArray(year: number, month: number) {
@@ -39,6 +68,21 @@ function getValidDayArray(year: number, month: number) {
   return days;
 }
 
+function getWeekdayContent() {
+  const htmlArray = [];
+  const weekdays = translation.weekdays.split(",");
+  let count = 0;
+  for (let weekday of weekdays) {
+    htmlArray.push(
+      `<div id='selection__datepicker__weekday-${count}' class="selection__datepicker__weekday">
+        ${weekday}
+       </div>`
+    );
+    count++;
+  }
+  return htmlArray.join(" ");
+}
+
 function getDatePickerContent(year: number, month: number) {
   console.log(files);
   const htmlArray = [];
@@ -48,13 +92,13 @@ function getDatePickerContent(year: number, month: number) {
     let id;
     const dayInvalid = day === "";
     if (dayInvalid) {
-      id = "invalid-" + invalidFieldCount;
+      id = "__invalid-" + invalidFieldCount;
       invalidFieldCount++;
     } else {
-      id = day;
+      id = "-" + day;
     }
     htmlArray.push(
-      `<button id='${id} class="selection__datepicker__day${
+      `<button id='selection__datepicker__day${id} class="selection__datepicker__day${
         dayInvalid ? " selection__datepicker__day__invalid" : ""
       }">${day}</button>`
     );
